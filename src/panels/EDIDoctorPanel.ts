b1f0b5b31485fe97002691b7724aec5b4a372515
Milos Zivkovic <developer.milos.zivkovic@gmail.com>
2022-12-18 03:15:35 +0100

    Added Svelte webview

---
+++ src/panels/EDIDoctorPanel.ts
@@ -0,0 +1,164 @@
+import * as vscode from "vscode";
+import { getNonce } from "../utils/nonce";
+
+export class EDIDoctorPanel {
+  /**
+   * Track the currently panel. Only allow a single panel to exist at a time.
+   */
+  public static currentPanel: EDIDoctorPanel | undefined;
+
+  public static readonly viewType = "swiper";
+
+  private readonly _panel: vscode.WebviewPanel;
+  private readonly _extensionUri: vscode.Uri;
+  private _disposables: vscode.Disposable[] = [];
+
+  public static createOrShow(extensionUri: vscode.Uri) {
+    const column = vscode.window.activeTextEditor
+      ? vscode.window.activeTextEditor.viewColumn
+      : undefined;
+
+    // If we already have a panel, show it.
+    if (EDIDoctorPanel.currentPanel) {
+      EDIDoctorPanel.currentPanel._panel.reveal(column);
+      EDIDoctorPanel.currentPanel._update();
+      return;
+    }
+
+    // Otherwise, create a new panel.
+    const panel = vscode.window.createWebviewPanel(
+      EDIDoctorPanel.viewType,
+      "VSinder",
+      column || vscode.ViewColumn.One,
+      {
+        // Enable javascript in the webview
+        enableScripts: true,
+
+        // And restrict the webview to only loading content from our extension's `media` directory.
+        localResourceRoots: [
+          vscode.Uri.joinPath(extensionUri, "media"),
+          vscode.Uri.joinPath(extensionUri, "out/compiled"),
+        ],
+      }
+    );
+
+    EDIDoctorPanel.currentPanel = new EDIDoctorPanel(panel, extensionUri);
+  }
+
+  public static kill() {
+    EDIDoctorPanel.currentPanel?.dispose();
+    EDIDoctorPanel.currentPanel = undefined;
+  }
+
+  public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    EDIDoctorPanel.currentPanel = new EDIDoctorPanel(panel, extensionUri);
+  }
+
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+    this._extensionUri = extensionUri;
+
+    // Set the webview's initial html content
+    this._update();
+
+    // Listen for when the panel is disposed
+    // This happens when the user closes the panel or when the panel is closed programatically
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+    // // Handle messages from the webview
+    // this._panel.webview.onDidReceiveMessage(
+    //   (message) => {
+    //     switch (message.command) {
+    //       case "alert":
+    //         vscode.window.showErrorMessage(message.text);
+    //         return;
+    //     }
+    //   },
+    //   null,
+    //   this._disposables
+    // );
+  }
+
+  public dispose() {
+    EDIDoctorPanel.currentPanel = undefined;
+
+    // Clean up our resources
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
+
+  private async _update() {
+    const webview = this._panel.webview;
+
+    this._panel.webview.html = this._getHtmlForWebview(webview);
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // // And the uri we use to load this script in the webview
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled", "EDIDoctor.js")
+    );
+
+    // Local path to css styles
+    const styleResetPath = vscode.Uri.joinPath(
+      this._extensionUri,
+      "media",
+      "reset.css"
+    );
+    const stylesPathMainPath = vscode.Uri.joinPath(
+      this._extensionUri,
+      "media",
+      "vscode.css"
+    );
+
+    // Uri to load styles into webview
+    const stylesResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        "media",
+        "reset.css"
+      )
+    );
+    const stylesMainUri = webview.asWebviewUri(vscode.Uri.joinPath(
+      this._extensionUri,
+      "media",
+      "vscode.css"
+    ));
+    const cssUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "out", "compiled/swiper.css")
+    );
+
+    // Use a nonce to only allow specific scripts to be run
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+        -->
+        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${
+      webview.cspSource
+    }; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+        <link href="${cssUri}" rel="stylesheet">
+        <script nonce="${nonce}">
+            
+        </script>
+			</head>
+      <body>
+			</body>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</html>`;
+  }
+}
